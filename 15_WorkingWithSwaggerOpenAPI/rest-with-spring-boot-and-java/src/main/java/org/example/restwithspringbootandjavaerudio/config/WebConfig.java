a803772cfa6ad2f9e46eebc41c37efd6c733800e
Rodrigo <rdg.hora1@gmail.com>
2024-05-28 11:45:07 -0300

    Revert "CORS Configuration"
    This reverts commit 977498396249c5904cfa12c3ac7bf7450864bbd5.

--- 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java/src/main/java/org/example/restwithspringbootandjavaerudio/config/WebConfig.java
+++ 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java/src/main/java/org/example/restwithspringbootandjavaerudio/config/WebConfig.java
@@ -2,12 +2,10 @@ package org.example.restwithspringbootandjavaerudio.config;
 
 
 import org.example.restwithspringbootandjavaerudio.serialization.converter.YamlJackson2HttpMessageConverter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,22 +14,9 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
 
-    @Value("${cors.originPatterns:default}")
-    private String corsOriginsPatterns = "";
-
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new YamlJackson2HttpMessageConverter());
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        var allowedOrigins = corsOriginsPatterns.split(",");
-        registry.addMapping("/**")
-//                .allowedMethods("GET", "POST", "PUT")
-                .allowedOrigins(allowedOrigins)
-                .allowedMethods("*")
-                .allowCredentials(true);
     }
 
     @Override
