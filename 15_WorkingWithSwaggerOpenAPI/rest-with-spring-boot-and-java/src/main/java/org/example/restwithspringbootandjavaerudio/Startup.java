d7acbcce777a0d1c987cb0c375137cbfb79f7f7e
Rodrigo <rdg.hora1@gmail.com>
2024-05-28 09:53:49 -0300

    Swagger adding completed

--- 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java/src/main/java/org/example/restwithspringbootandjavaerudio/Startup.java
+++ 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java/src/main/java/org/example/restwithspringbootandjavaerudio/Startup.java
@@ -2,8 +2,13 @@ package org.example.restwithspringbootandjavaerudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+        @PropertySource(value = "classpath:application.yml")
+})
 public class Startup {
 
     public static void main(String[] args) {
---
+++ 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java/src/test/java/org/example/restwithspringbootandjavaerudio/configs/TestConfigs.java
@@ -0,0 +1,5 @@
+package org.example.restwithspringbootandjavaerudio.configs;
+
+public class TestConfigs {
+    public static final int SERVER_PORT = 8888;
+}
